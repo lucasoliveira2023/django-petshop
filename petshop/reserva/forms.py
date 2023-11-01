a20dacd50afaf43e8b1eed934f956d059b31d720
Lucas Oliveira <blesslucas89@gmail.com>
2023-11-01 19:26:17 -0400

    atualização modulo6 semnaan4

--- petshop/reserva/forms.py
+++ petshop/reserva/forms.py
@@ -1,24 +1,30 @@
 from datetime import date
 from django import forms
 from reserva.models import Reserva
-#atividade da semana m6 semana4
+##atividade do modulo 6 semana4
 class ReservaForm(forms.ModelForm):
     
-    def clean_data(self):
-        data = self.cleaned_data['date']
-        hoje = date.today()
-        if data < hoje:
-            raise forms.ValidationError('impossivel fazer reserva para o passado')
-        
-        reserva_existem = Reserva.objects.filter(data=data).count()
-        if reserva_existem >= 4:
-            raise forms.ValidationError('ja existem 4 reservas para esta data. Por favor escolha outra data.')
-        
-        return data
-
     class Meta:
         model = Reserva
         fields = [
             'nome', 'email', 'nome_pet', 'data', 'turno',
             'tamanho', 'observacoes'
         ]
+        widget = {
+            'DiadaReserva': forms.DateInput(attrs={'type': 'date'}),
+        }
+    
+    def clean_data(self):
+        data = self.cleaned_data['date']
+        hoje = date.today()
+        
+        if data < hoje:
+            raise forms.ValidationError('não é possivel realizar uma reserva para o passado!')
+        
+        numeroAtualdeReserva = Reserva.objects.filter(DiadaReserva=data).count()
+        
+        if numeroAtualdeReserva >=4:
+            raise forms.ValidationError('ja existem muitas reservas para esse dia, por favor selecione outro dia')
+        
+        return data
+    
